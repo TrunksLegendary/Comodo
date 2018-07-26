4e8c005fc313040875adcf708ea1cd58e59cdd43
Hiren Patel <trunks0408@gmail.com>
2018-07-26 13:10:19 -0400

    Update Nurd-Backup.cmd

--- OldSchool-Windows_Batch/Nurd-Backup.cmd
+++ OldSchool-Windows_Batch/Nurd-Backup.cmd
@@ -1,3 +1,8 @@
+
+# this will copy file and subfolders from my oneDrive folder to a backup location
+# I run a task scheudaler set to every 30 minutes 
+# files are overwritten
+
 SET _datetime=%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4%
 if not exist "C:\Users\Public\Comodo-Backup\" (mkdir "C:\Users\Public\Comodo-Backup")
 robocopy "C:\Users\hirenp\OneDrive - Nurd" "C:\Users\Public\Comodo-Backup\OneDrive - Nurd" /E /R:1 /LOG+:C:\Users\Public\Comodo-Backup\RoboSync_%_DATETIME%.log /V
